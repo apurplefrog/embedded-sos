54327c3bbfcc54b25ec504efcb0d7bd24d6cb0f6
purple frog <theodorebaker08@gmail.com>
2024-12-26 23:22:04 -0500

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+build with 
+`cargo build --Zbuild-std`
+then run
+`sudo probe-rs run target/thumbv7em-none-eabihf/debug/stm32-rust --chip STM32F446RETx`
---
+++ src/main.rs
@@ -0,0 +1,51 @@
+//! Blinks an LED
+
+#![deny(unsafe_code)]
+#![deny(warnings)]
+#![no_main]
+#![no_std]
+
+use crate::hal::{pac, prelude::*};
+use cortex_m_rt::entry;
+use panic_halt as _;
+use stm32f4xx_hal as hal;
+
+const DOT_LENGTH: u32 = 10_000;
+const PAUSE_LENGTH: u32 = 10_000;
+const LONG_PAUSE_LENGTH: u32 = 20_000;
+const LINE_LENGTH: u32 = 20_000;
+
+#[entry]
+fn main() -> ! {
+    let message = "...___... ";
+    let p = pac::Peripherals::take().unwrap();
+
+    let gpioa = p.GPIOA.split();
+    let mut led = gpioa.pa1.into_push_pull_output();
+
+    loop {
+        for character in message.chars().into_iter() {
+            match character {
+                '.' => {
+                    for _ in 1..DOT_LENGTH {
+                        led.set_high();
+                    }
+                }
+                '_' => {
+                    for _ in 1..LINE_LENGTH {
+                        led.set_high();
+                    }
+                }
+                ' ' => {
+                    for _ in 1..LONG_PAUSE_LENGTH {
+                        led.set_low();
+                    }
+                }
+                _ => {}
+            }
+            for _ in 1..PAUSE_LENGTH {
+                led.set_low();
+            }
+        }
+    }
+}
